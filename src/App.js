e7639cfece1b9f4cedf530d94d70c4114d01af56
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-02-11 04:06:46 +0600

    manipulatingWithReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const containerStyles = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '1rem',
+    width: '60vw',
+    margin: '0 auto'
+  };
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  },[])
+
+  console.log(users);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={containerStyles} className="products__container">
+          {
+            users.map(user => {
+              return <UserCard user={user}></UserCard>
+            })
+          }
+        </div>
+        {/* <Counter></Counter> */}
       </header>
     </div>
   );
 }
+
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
+//   const handleCount = () => setCount(count + 1);
+//   return (
+//     <div>
+//       <h2>Counter : {count}</h2>
+//       <button onClick={handleCount}>Increase state value</button>
+//     </div>
+//   );
+// };
+
+
+function UserCard(props) {
+  const nameStyles = {
+    color: '#FFFFFF',
+    border: 'solid 2px maroon',
+    borderRadius: '.5rem',
+    padding: '2rem',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  return (
+    <div style={nameStyles} className="productCard">
+      <h2>{props.user.name}</h2>
+      <h3>UserID : {props.user.id}</h3>
+    </div>
+  );
+};
 
 export default App;
